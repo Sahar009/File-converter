001886eb4e19dd8f8f0145f47bd3bafd6789ff31
Eniwealth09 <102475341+Eniwealth09@users.noreply.github.com>
2023-08-15 16:06:51 +0100

    Add files via upload

---
+++ File-converter.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from tkinter import filedialog
+import PyPDF2
+import document
+
+
+def convert_pdf_to_word():
+    pdf_file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+    
+    if pdf_file_path:
+        pdf_text = ""
+        
+    pdf_file = open(pdf_file, 'rb')
+    pdf = PyPDF2.PdfReader(pdf_file)
+            
+    for page_num in range(pdf.numPages):
+                page = pdf.getPage(page_num)
+                pdf_text += page.extractText()
+            
+    pdf_file.close()
+            
+    doc = convert_pdf_to_word()
+    doc.add_paragraph(pdf_text)
+            
+    word_file = pdf_file.replace(".pdf", ".docx")
+    doc.save(word_file)
+            
+    result_label.config(text=f"Conversion complete. Saved as {word_file}")
+    
+    
+    result_label.config(text="Error during conversion.")
+
+# Create the main window
+root = tk.Tk()
+root.title("PDF to Word Converter")
+
+# Create and place widgets
+convert_button = tk.Button(root, text="Convert PDF to Word", command=convert_pdf_to_word)
+convert_button.pack(pady=20)
+
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+# Start the main loop
+root.mainloop()
